c0a8da6ce8d22fa86f0dd4f3c50f2b837ffbf25f
Ali <aaliyev14291@ada.edu.az>
2025-10-24 00:48:19 +0400

    Input Processor is done.

--- src/InputProcessor.java
+++ src/InputProcessor.java
@@ -36,9 +36,37 @@ public class InputProcessor {
                         Account acc = AccountManager.getInstance().createAccount(owner, c, amt, pin);
                         bw.write("CREATED|" + acc.getId() + "|" + owner + System.lineSeparator());
                         break;
-
+                    case "TRANSFER":
+                        if (parts.length < 6) {
+                            bw.write("BAD TRANSFER: " + line + System.lineSeparator());
+                            break;
+                        }
+                        int from = Integer.parseInt(parts[1]);
+                        int to = Integer.parseInt(parts[2]);
+                        double amount = Double.parseDouble(parts[3]);
+                        Currency currency = Currency.valueOf(parts[4].toUpperCase());
+                        int pin2 = Integer.parseInt(parts[5]);
+                        executor.submit(() -> {
+                            boolean ok = AccountManager.getInstance().verifyPinAndTransfer(from, pin2, to, amount, currency);
+                            try {
+                                synchronized (bw) {
+                                    bw.write("TRANSFER|" + from + "|" + to + "|" + amount + "|" + currency + "|" + (ok ? "OK" : "FAILED") + System.lineSeparator());
+                                    bw.flush();
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+                        break;
+                    default:
+                        bw.write("UNKNOWN CMD: " + line + System.lineSeparator());
                 }
             }
+            bw.write("BATCH_PROCESS_COMPLETE" + System.lineSeparator());
+            System.out.println("Input processing submitted tasks; results will be in: " + outputFilePath);
+        } catch (Exception e) {
+            System.err.println("Error processing input: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
