ab342b7df03c8aea07f1846124d5fad1e3193a28
Ali <aaliyev14291@ada.edu.az>
2025-10-25 02:36:21 +0400

    Output Writer done.

--- src/OutputWriter.java
+++ src/OutputWriter.java
@@ -28,9 +28,23 @@ public class OutputWriter {
                 for (Account a : accounts) {
                     sb.append(a.toString()).append(System.lineSeparator());
                     sb.append("Balances detail:").append(System.lineSeparator());
-
+                    for (Map.Entry<Currency, Double> e : a.snapshotBalances().entrySet()) {
+                        sb.append("  ")
+                                .append(e.getKey())
+                                .append(": ")
+                                .append(String.format("%.2f", e.getValue()))
+                                .append(System.lineSeparator());
+                    }
                 }
             }
+            sb.append(System.lineSeparator()).append("=== Transactions ===").append(System.lineSeparator());
+            for (Transaction t : TransactionHistory.getInstance().snapshot()) {
+                sb.append(t.toString()).append(System.lineSeparator());
+            }
+            bw.write(sb.toString());
+            System.out.println("Snapshot saved to " + path);
+        } catch (IOException e) {
+            System.err.println("Failed to write snapshot: " + e.getMessage());
         }
     }
 }
